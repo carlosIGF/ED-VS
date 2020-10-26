9dc79fa3308a2f25e978eac8ea2bb44bf49fa4d0
CarlosIGF <carlos.igformacion@gmail.com>
2020-10-26 13:37:15 +0100

    Proyecto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 3;
+            int y = 2;
+            if (x > 2)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
